8196cbf43349dab792a7213f728f08950ac514ae
Nikita Dolgikh <n.dolgikh.020@mail.ru>
2025-11-03 14:17:51 +0300

    изменил пэкедж

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MBCEX/',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+    base: '/MBCEX/',
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Разделяем vendor библиотеки на отдельные чанки
+                    'react-vendor': ['react', 'react-dom'],
+                    'lottie-vendor': ['@lottiefiles/dotlottie-react'],
+                },
+            },
+        },
+        // Увеличиваем лимит предупреждения до 1000 KB
+        chunkSizeWarningLimit: 1000,
+    },
 })
